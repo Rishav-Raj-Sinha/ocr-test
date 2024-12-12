977e91d36278dd467f1273dabbd78e96b37b4790
Rishav-Raj-Sinha <rajrishav028@gmail.com>
2024-12-13 04:53:29 +0530

    minor

--- main.py
+++ main.py
@@ -11,13 +11,7 @@ hide_decoration_bar_style = '''<style>header {visibility: hidden;}
 </style><style>footer{visibility: hidden;}</style>'''
 st.markdown(hide_decoration_bar_style, unsafe_allow_html=True)
 
-picture = st.camera_input("Take a picture")
-if picture :
-        st.image(picture)
-        image = Image.open(picture)
-        reader = easyocr.Reader(['en']) # this needs to run only once to load the model into memory
-        text = reader.readtext(image,detail = 0)
-        st.write(text[0])
+
 
 data = pd.read_csv("Medicine_Details.csv")
 #print(data.head())
@@ -30,7 +24,15 @@ df_se = "Side_effects"
 
 st.title("HealthNet")
 with st.container(border =True,height = 750):
-    with st.container(border = True,height = 700):
+    with st.container(border = True,height = 400):
+        picture = st.camera_input("Take a picture")
+        if picture :
+            #st.image(picture)
+            image = Image.open(picture)
+            reader = easyocr.Reader(['en']) # this needs to run only once to load the model into memory
+            text = reader.readtext(image,detail = 0)
+            #st.write(text[0])
+    with st.container(border = True,height = 400):
         prompt = text[0] # "prompt" variable takes the user input
 
         if prompt:
